4618c94ca7a668ce1457fb14b7454e930286e128
Christiano Walter <125219293+ChristianoWalter@users.noreply.github.com>
2024-03-22 03:19:30 -0300

    Initial commit

---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    string palavra = "palavra";
+    bool binario = true;
+    int numeroInteiro = 1;
+    float numeroQuebrado = 1.5f;
+    Vector2 doisVetores;
+    Transform tamanhoLocalDirecao;
+    enum selecaoEnumerada { um, dois, tres, quatro};
+    List<selecaoEnumerada> listas;
+
+    GameObject objeto;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
