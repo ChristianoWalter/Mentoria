53cb3e16018f8ec21e3b1d0f94c24966d87526a9
Christiano Walter <chris.w.lemos03@gmail.com>
2024-04-24 19:25:09 -0300

    modulo 19 e 20

--- Assets/Scripts/CoreGameplay/GameController.cs
+++ Assets/Scripts/CoreGameplay/GameController.cs
@@ -1,12 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class GameController : MonoBehaviour
 {
     public static GameController instance;
 
     public GameObject gameOverPanel;
+    public GameObject pausePanel;
+    public bool isPaused;
 
     private void Awake()
     {
@@ -17,6 +21,30 @@ public class GameController : MonoBehaviour
         yield return new WaitForSeconds(1f);
         gameOverPanel.SetActive(true);
         Time.timeScale = 0f;
+    }
+
+    public void PauseAction(InputAction.CallbackContext value)
+    {
+        if (value.performed)
+        {
+            PauseUnpause();
+        }
+    }
+
+    public void PauseUnpause()
+    {
+        if (isPaused)
+        {
+            Time.timeScale = 1f;
+            pausePanel.SetActive(false);
+            isPaused = false;
+        }
+        else
+        {
+            Time.timeScale = 0f;
+            pausePanel.SetActive(true);
+            isPaused = true;
+        }
     }
 
     public void GameOver()
